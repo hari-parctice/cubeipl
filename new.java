da07d9287cdccac167b01a5b8627d7797fac7d55
hari-parctice <miriyalahareesh2684@gmail.com>
2024-12-18 20:25:32 +0530

    log.py added by dev

---
+++ new.java
@@ -0,0 +1,16 @@
+[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C> login.java
+public class Main {
+    public static void main(String[] args)
+    {
+
+        // initializing array
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        // size of array
+        int n = arr.length;
+
+        // traversing array
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
